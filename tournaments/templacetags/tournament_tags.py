f3377798dab8450b328f8c1f128bcbce07090857
Dan Sandoval <dsandovalmail@gmail.com>
2025-03-15 19:41:56 -0400

    Minor bug fix

--- tournaments/templacetags/tournament_tags.py
+++ tournaments/templacetags/tournament_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 def sum_wagers(wagers):
     """Calculate the sum of wager points"""
     return sum(wager.points for wager in wagers)
+
+@register.filter
+def get_item(dictionary, key):
+    """Get an item from a dictionary using a variable key"""
+    return dictionary.get(key)
